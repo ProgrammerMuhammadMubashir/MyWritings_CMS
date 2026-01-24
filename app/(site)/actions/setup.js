51383e458f5faee1d791c9bd299bd41301a6316d
MuhammadMubashir <itismubashiratcode@gmail.com>
2026-01-24 21:05:32 +0500

    Fixing mongodb server connection

--- app/(site)/actions/setup.js
+++ app/(site)/actions/setup.js
@@ -1,49 +1,54 @@
-"use server"
-import { NextResponse } from "next/server"
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connectDB = async () => {
+let isConnected = false;
+
+export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.URL);
-    console.log('✅ MongoDB connected successfully to local database.');
+    await mongoose.connect(process.env.URL, {
+      bufferCommands: false,
+    });
+    isConnected = true;
+    console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error('❌ MongoDB connection error:', error.message);
-    process.exit(1);
+    console.error("❌ MongoDB connection error:", error);
+    throw error;
   }
 };
-connectDB()
+
 const commentSchema = new mongoose.Schema({
-    commenter:{
-        type: String, 
-    },
-    comment: {
-        type: String, 
-    },
-    createdAt: {
-        type: Date, 
-    }
+  commenter: String,
+  comment: String,
+  createdAt: Date,
 });
 
 const articleSchema = new mongoose.Schema({
   title: String,
-  slug:String,
+  slug: String,
   category: String,
   content: String,
   author: String,
-  publish_date: String, 
+  publish_date: String,
   tags: String,
-  comments: [commentSchema] 
-},
-)
-const statsSchema= new mongoose.Schema({
-  total:Number,
-  PV:Number,
-  Engage:Number,
-})
-const settingSchema=new mongoose.Schema({
-  theme:String,
-})
+  comments: [commentSchema],
+});
 
-export const collectionArticles = mongoose.models.Article || mongoose.model('Article', articleSchema);
-export const collectionStats = mongoose.models.Stats || mongoose.model('Stats', statsSchema);
-export const collectionSetting = mongoose.models.Setting || mongoose.model('Setting', settingSchema);
+const statsSchema = new mongoose.Schema({
+  total: Number,
+  PV: Number,
+  Engage: Number,
+});
+
+const settingSchema = new mongoose.Schema({
+  theme: String,
+});
+
+export const collectionArticles =
+  mongoose.models.Article || mongoose.model("Article", articleSchema);
+
+export const collectionStats =
+  mongoose.models.Stats || mongoose.model("Stats", statsSchema);
+
+export const collectionSetting =
+  mongoose.models.Setting || mongoose.model("Setting", settingSchema);
