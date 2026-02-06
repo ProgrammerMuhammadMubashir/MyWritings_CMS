2af5256b7815e04fb22251ddab624abdcdfef7cc
MuhammadMubashir <itismubashiratcode@gmail.com>
2026-02-06 17:33:42 +0500

    Revert "Deployment .. URL Resolved"
    This reverts commit 6a26e35fb31426a5adf3f0f94d1a10d9981931d1.

--- components/Admin_Shower.js
+++ components/Admin_Shower.js
@@ -8,10 +8,7 @@ import { numContext } from '@/app/(admin)/Admin/page'
 import Theme_handler from './Theme_handler'
 
 const Admin_Shower = () => {
-
-  
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
-
+const BASE_URL =process.env.NEXT_PUBLIC_API_URL || ""
 
   const router=useRouter()
   let [art,setArt]=useState([])
